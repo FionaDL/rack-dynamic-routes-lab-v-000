e59fa58a8d84e039b47a823954792e015f824f80
Fiona Lapham <fionadlapham@gmail.com>
2018-06-12 07:59:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,16 @@
 require 'pry'
 
 class Application
-  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
+  @@items = [Item.new(name: "Figs", price: 3.42),Item.new(name: "Pears", price: 0.99)]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-        this_item = req.params["item"]
+      item = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+       if item == @@items.find{|i| i.name == item
+
+        resp.write song.artist
         binding.pry
          if @@items.include?(this_item)
            resp.write "This #{this_item} costs #{this_item}"
