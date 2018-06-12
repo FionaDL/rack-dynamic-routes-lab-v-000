a3eab7fa90b817a814de4ff127fc2c71a50d1cfc
Fiona Lapham <fionadlapham@gmail.com>
2018-06-12 07:53:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,10 +7,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-        this_item = req.params["item name"]
+        this_item = req.params["item"]
         binding.pry
          if @@items.include?(this_item)
-           resp.write "This #{this_item.name} costs #{new_item.price}"
+           resp.write "This #{this_item} costs #{this_item}"
          else
          resp.write "Couldn't find item"
          resp.status = 400
