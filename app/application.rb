d429030321b3960c3f32164ed33f4c6f92d6e1f9
Fiona Lapham <fionadlapham@gmail.com>
2018-06-12 07:47:11 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Application
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+        this_item = req.path.split("/items/").last
+         if @@items.include?(this_item)
+           resp.write "This #{this_item.name} costs #{new_item.price}"
+         else
+         resp.write "Couldn't find item"
+         resp.status = 400
+       end
+    else
+       resp.write "Route not found"
+       resp.status = 404
+    end
+    resp.finish
+  end
+end
