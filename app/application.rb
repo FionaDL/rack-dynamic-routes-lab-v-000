a8e7c092a1cbb25b21b80547848c4c795150ae14
Fiona Lapham <fionadlapham@gmail.com>
2018-06-12 08:05:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-      item = req.path.split("/items/").last
-       if item == @@items.find{|i| i.name == item}
+      item_name = req.path.split("/items/").last
+       if item = @@items.find{|i| i.name == item_name}
          resq.write item.name
        else
          resp.write "Couldn't find item"
