ceb5f51290f5fd4eddb0dddb6bd519da081c96b3
Fiona Lapham <fionadlapham@gmail.com>
2018-06-12 08:01:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      
+       binding.pry
        if item == @@items.find{|i| i.name == item}
          item.price
        else
